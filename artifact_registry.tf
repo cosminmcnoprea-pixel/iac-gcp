f345cbf278ec567ae91370dab226c77aa385a417
Cosmin Oprea <cosmin.mcn.oprea@gmail.com>
2025-12-16 01:37:17 +0200

    Allow prod iac to pull images from dev to promote

--- artifact_registry.tf
+++ artifact_registry.tf
@@ -7,3 +7,14 @@ resource "google_artifact_registry_repository" "docker_repo" {
 
   depends_on = [google_project_service.services]
 }
+
+# Grant prod service account read access to dev artifact registry (only in dev)
+resource "google_artifact_registry_repository_iam_member" "prod_reader" {
+  count = var.environment == "dev" ? 1 : 0
+
+  project    = var.project_id
+  location   = var.region
+  repository = google_artifact_registry_repository.docker_repo.name
+  role       = "roles/artifactregistry.reader"
+  member     = "serviceAccount:service-account-iac@global-reach-media-prod.iam.gserviceaccount.com"
+}
