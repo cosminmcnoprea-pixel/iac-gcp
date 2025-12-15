66af4f9d3d08fd6158f4b8256e0c88d1522cd68c
Cosmin Oprea <cosmin.mcn.oprea@gmail.com>
2025-12-15 22:40:53 +0200

    Enable services and split the plan and apply to have 2 separate workflows

--- main.tf
+++ main.tf
@@ -1,4 +1,19 @@
+locals {
+  services = [
+    "sqladmin.googleapis.com",
+  ]
+}
+
+resource "google_project_service" "services" {
+  for_each = toset(local.services)
+  project  = var.project_id
+  service  = each.value
+
+  disable_on_destroy = false
+}
+
 resource "google_sql_database_instance" "sql_instance" {
+  depends_on       = [google_project_service.services]
   name             = var.instance_name
   project          = var.project_id
   region           = var.region
